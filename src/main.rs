07823a1cc029d632c3b87a447e987d30b1f99391
Proudmuslim <proudmuslimos@protonmail.ch>
2021-09-07 07:41:11 -0700

    Still broken but cleaned up a bit anyway

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-extern crate serde_json;
-extern crate skyblock_rs as skyblock;
-extern crate tokio;
-
-use skyblock::*;
+use skyblock_rs::*;
 use std::env;
-use std::error::Error;
 use std::vec::Vec;
 
 #[tokio::main]
@@ -13,19 +8,11 @@ async fn main() -> Result<()> {
     let mut api = SkyblockApi::singleton(&api_key);
 
     let mut vec: Vec<Auction> = Vec::new();
-    let futa = api.iter_active_auctions(|auction| {
-        vec.push(auction.clone());
+    api.iter_active_auctions(|auction| {
+        vec.push(auction);
         Ok(())
-    });
+    }).await.unwrap();
 
-    let auctions = futa.await.unwrap_or_else(|error| {
-        eprintln!("Error {}", error);
-    });
-
-    // let json = serde_json::to_string_pretty(&auctions)?;
-    // let v = vec.push(auctions.clone());
-    // println!("{:#?}", v);
-    println!("{:#?}", auctions);
     println!("{:#?}", vec);
 
     Ok(())
