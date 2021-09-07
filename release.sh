40b020ae8293060f1c5f80216271f6b560ddc57b
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-09-07 15:14:30 +0200

    pain part 2

--- release.sh
+++
@@ -1,6 +0,0 @@
-mkdir dist
-yarn bundle
-cp target/release/lowesbins dist/
-cp lowesbtins.js dist/
-
-cp lowesbtins.json dist/
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,104 @@
-extern crate serde_json;
-extern crate skyblock_rs as skyblock;
-extern crate tokio;
+#![deny(warnings)]
 
-use skyblock::*;
-
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{header, Body, Method, Request, Response, Result, Server, StatusCode};
 use std::env;
-use std::error::Error;
+use std::future::Future;
+use std::process::Command;
+use tokio::fs::File;
+use tokio::time::{self, Duration};
+use tokio_util::codec::{BytesCodec, FramedRead};
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let api_key = env::var("API_KEY")?;
-    let mut api = SkyblockApi::singleton(&api_key);
+static INDEX: &str = "lowestbins.json";
+static NOTFOUND: &[u8] = b"Not Found";
 
-    // api.iter_active_auctions(api);
-    // let mut test = "";
-    let mut t: std::ops::Fn<Auction>;
-    let futa = api.iter_active_auctions(&t);
-
-    let auctions = futa.await?;
-
-    let json = serde_json::to_string_pretty(&auctions)?;
-
-    println!("{}", json);
+fn auctions() -> std::io::Result<()> {
+    println!("test");
+    let path = env::current_dir()?;
+    println!("{}", path.display());
+    Command::new("node")
+        .arg("lowestbins.js")
+        .spawn()
+        .expect("failed to execute process");
 
     Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let addr = "127.0.0.1:1337".parse().unwrap();
+
+    let make_service =
+        make_service_fn(|_| async { Ok::<_, hyper::Error>(service_fn(response_examples)) });
+
+    let server = Server::bind(&addr).serve(make_service);
+
+    println!("Listening on http://{}", addr);
+
+    auctions().expect("error");
+    set_interval(
+        || async { auctions().expect("error") },
+        Duration::from_millis(300000),
+    );
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+async fn response_examples(req: Request<Body>) -> Result<Response<Body>> {
+    println!("Request");
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/lowestbins.json") | (&Method::GET, "/lowestbins") => {
+            println!("/ GET");
+            // Response::builder().header(header::CONTENT_TYPE, "application/json").body(simple_file_send(INDEX));
+            simple_file_send(INDEX).await
+        }
+        _ => Ok(not_found()),
+    }
+}
+
+/// HTTP status code 404
+fn not_found() -> Response<Body> {
+    Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(NOTFOUND.into())
+        .unwrap()
+}
+
+async fn simple_file_send(filename: &str) -> Result<Response<Body>> {
+    // Serve a file by asynchronously reading it by chunks using tokio-util crate.
+
+    if let Ok(file) = File::open(filename).await {
+        let stream = FramedRead::new(file, BytesCodec::new());
+        let body = Body::wrap_stream(stream);
+
+        return Ok(Response::builder()
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(body)
+            .unwrap());
+    }
+
+    Ok(not_found())
+}
+
+fn set_interval<F, Fut>(mut f: F, dur: Duration)
+where
+    F: Send + 'static + FnMut() -> Fut,
+    Fut: Future<Output = ()> + Send + 'static,
+{
+    // Create stream of intervals.
+    let mut interval = time::interval(dur);
+    tokio::spawn(async move {
+        // Skip the first tick at 0ms.
+        interval.tick().await;
+        loop {
+            // Wait until next tick.
+            interval.tick().await;
+            // Spawn a task for this tick.
+            tokio::spawn(f());
+        }
+    });
 }
